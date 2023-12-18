071d2e8c639598d4f96b4e50e8e1150c215980f4
kathiresan916 <kathiresan_s916@yahoo.com>
2023-12-18 18:00:53 +0530

    2.0 Upgraded

---
+++ Want Upgrade/test.py
@@ -0,0 +1,12 @@
+class Car:
+    def start_engine(self):
+        print("Car Started")
+
+
+class ElectricCar(Car):
+    def charge_battery(self):
+        print("Battery Charging")
+
+
+my_car = ElectricCar()
+my_car.start_engine()
